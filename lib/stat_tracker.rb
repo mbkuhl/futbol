9a66600c2fa1e36df5d4fb62893d4bdd91213b2b
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-09-24 11:17:19 -0600

    Refactor winningest and worst coach to work with new dataset

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -129,41 +129,11 @@ class StatTracker
   end
 
   def winningest_coach(season)
-    coach_stats = Hash.new { |hash, coach_name| hash[coach_name] = { wins: 0, games: 0 } }
-    game_teams.each do |game_team|
-      game_id = game_team[:game_id]
-      coach = game_team[:head_coach]
-      result = game_team[:result]
-      if (games = game.find { |g| g[:game_id] == game_team[:game_id] && g[:season] == season })
-        coach_stats[coach][:wins] += 1 if result == "WIN"
-        coach_stats[coach][:games] += 1
-      end
-    end
-    coach_win_percentages = coach_stats.transform_values do |stats|
-      (stats[:wins].to_f / stats[:games]).round(2)
-    end
-    coach_win_percentages.key(coach_win_percentages.values.max)
+    @seasons[season].winningest_coach
   end
     
   def worst_coach(season)
-    coach_stats = Hash.new { |hash, coach_name| hash[coach_name] = { wins: 0, games: 0 } }
-    game_teams.each do |game_team|
-      game_id = game_team[:game_id]
-      coach = game_team[:head_coach]
-      result = game_team[:result]
-      if (games = game.find { |g| g[:game_id] == game_team[:game_id] && g[:season] == season })
-        coach_stats[coach][:wins] += 1 if result == "WIN"
-        coach_stats[coach][:games] += 1
-      end
-    end
-    coach_win_percentages = coach_stats.transform_values do |stats|
-      if stats[:games] > 0
-        (stats[:wins].to_f / stats[:games]).round(2)
-      else
-        0.0
-      end
-    end
-    coach_win_percentages.key(coach_win_percentages.values.min)
+    @seasons[season].worst_coach
   end
 
   def most_accurate_team(season_id)
